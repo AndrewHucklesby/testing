55ec63b80b5b73c2f4e1e275030721dba2cce7d1
AndrewHucklesby <103017611+AndrewHucklesby@users.noreply.github.com>
2022-05-03 15:03:04 +1200

    Update test.sh

--- manifests/test.sh
+++ manifests/test.sh
@@ -75,7 +75,7 @@ main() {
     [[ "$(command -v microk8s kubectl)" ]] || errorAndExit "Unable to find kubectl binary. Please ensure kubectl is installed before running this script."
     
     info "Downloading agent manifest..."
-    curl -L  https://downloads.portainer.io/ce2-13/portainer-agent-edge-k8s.yaml -o portainer-agent-edge-k8s.yaml || errorAndExit "Unable to download agent manifest"
+    curl -L  https://raw.githubusercontent.com/AndrewHucklesby/testing/main/manifests/edge.yaml -o portainer-agent-edge-k8s.yaml || errorAndExit "Unable to download agent manifest"
     
     info "Creating Portainer namespace..."
     microk8s kubectl create namespace portainer
